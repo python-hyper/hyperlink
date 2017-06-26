9feffe0e785cb1a439f3dc57bf4366606f4520f0
Mahmoud Hashemi <mahmoud@hatnote.com>
2017-06-25 17:15:16 -0700

    improve assertRaises and associated coverage

--- hyperlink/test/common.py
+++ hyperlink/test/common.py
@@ -40,31 +40,19 @@ class HyperlinkTestCase(TestCase):
 class _AssertRaisesContext(object):
     "A context manager used to implement HyperlinkTestCase.assertRaises."
 
-    def __init__(self, expected, test_case, expected_regexp=None):
+    def __init__(self, expected, test_case):
         self.expected = expected
         self.failureException = test_case.failureException
-        self.expected_regexp = expected_regexp
 
     def __enter__(self):
         return self
 
     def __exit__(self, exc_type, exc_value, tb):
         if exc_type is None:
-            try:
-                exc_name = self.expected.__name__
-            except AttributeError:
-                exc_name = str(self.expected)
-            raise self.failureException("%s not raised" % exc_name)
+            exc_name = self.expected.__name__
+            raise self.failureException("%s not raised" % (exc_name,))
         if not issubclass(exc_type, self.expected):
             # let unexpected exceptions pass through
             return False
         self.exception = exc_value  # store for later retrieval
-        if self.expected_regexp is None:
-            return True
-
-        expected_regexp = self.expected_regexp
-        exc_str = str(exc_value)
-        if not expected_regexp.search(exc_str):
-            raise self.failureException('%r does not match %r' %
-                                        (expected_regexp.pattern, exc_str))
         return True
