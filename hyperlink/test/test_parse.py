ac3be792be623b123eed21bba8d1b583f75061df
Mahmoud Hashemi <mahmoud@hatnote.com>
2018-01-06 15:54:37 -0800

    fixing test coverage for parse

--- hyperlink/test/test_parse.py
+++ hyperlink/test/test_parse.py
@@ -2,38 +2,32 @@
 
 from __future__ import unicode_literals
 
+from .common import HyperlinkTestCase
 from hyperlink import parse, EncodedURL, DecodedURL
 
 BASIC_URL = 'http://example.com/#'
 TOTAL_URL = "https://%75%73%65%72:%00%00%00%00@xn--bcher-kva.ch:8080/a/nice%20nice/./path/?zot=23%25&zut#frég"
 UNDECODABLE_FRAG_URL = TOTAL_URL + '%C3'
 
-def test_parse():
-    purl = parse(TOTAL_URL)
-    assert isinstance(purl, DecodedURL)
-    assert purl.user == 'user'
-    assert purl.get('zot') == ['23%']
-    assert purl.fragment == 'frég'
 
-    purl2 = parse(TOTAL_URL, decoded=False)
-    assert isinstance(purl2, EncodedURL)
-    assert purl2.get('zot') == ['23%25']
+class TestURL(HyperlinkTestCase):
+    def test_parse(self):
+        purl = parse(TOTAL_URL)
+        assert isinstance(purl, DecodedURL)
+        assert purl.user == 'user'
+        assert purl.get('zot') == ['23%']
+        assert purl.fragment == 'frég'
 
-    try:
-        purl3 = parse(UNDECODABLE_FRAG_URL)
-    except UnicodeDecodeError:
-        pass
-    else:
-        assert False, 'expected UnicodeDecodeError due to parse(lazy=False) by default'
+        purl2 = parse(TOTAL_URL, decoded=False)
+        assert isinstance(purl2, EncodedURL)
+        assert purl2.get('zot') == ['23%25']
 
-    try:
+        with self.assertRaises(UnicodeDecodeError):
+            purl3 = parse(UNDECODABLE_FRAG_URL)
+
         purl3 = parse(UNDECODABLE_FRAG_URL, lazy=True)
-    except UnicodeDecodeError:
-        assert False, 'expected UnicodeDecodeError due to parse(lazy=True)'
 
-    try:
-        purl3.fragment
-    except UnicodeDecodeError:
-        pass
-    else:
-        assert False, 'expected UnicodeDecodeError due to undecodable fragment'
+        with self.assertRaises(UnicodeDecodeError):
+            purl3.fragment
+
+        return
