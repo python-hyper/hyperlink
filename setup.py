413d742ef06b1152676a4534a3756c6b9fc51ab8
Frazer McLean <frazer@frazermclean.co.uk>
2019-09-18 19:03:12 +0200

    Add PyPI trove classifier for license

--- setup.py
+++ setup.py
@@ -40,7 +40,8 @@ setup(name='hyperlink',
           'Programming Language :: Python :: 3.4',
           'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
-          'Programming Language :: Python :: Implementation :: PyPy', ]
+          'Programming Language :: Python :: Implementation :: PyPy',
+          'License :: OSI Approved :: MIT License', ]
       )
 
 """
