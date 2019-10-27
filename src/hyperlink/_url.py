e0cee8a995020d0f469196c9cc89b49df2dc43b8
Wilfredo Sánchez <wsanchez@wsanchez.net>
2019-10-26 20:34:42 -0700

    Move definition of inet_pton into its own file so that the Windows-specific treatment can happen without cluttering _url.py. Makes use of TYPE_CHECKING not add to the noise so much.

--- src/hyperlink/_url.py
+++ src/hyperlink/_url.py
@@ -20,50 +20,13 @@ import sys
 import string
 import socket
 from unicodedata import normalize
-try:
-    from socket import inet_pton
-except ImportError:
-    inet_pton = None  # defined below
+from ._socket import inet_pton
 try:
     from collections.abc import Mapping
 except ImportError:  # Python 2
     from collections import Mapping
 
 from idna import encode as idna_encode, decode as idna_decode
-
-
-if inet_pton is None:
-    # based on https://gist.github.com/nnemkin/4966028
-    # this code only applies on Windows Python 2.7
-    import ctypes
-
-    class SockAddr(ctypes.Structure):
-        _fields_ = [("sa_family", ctypes.c_short),
-                    ("__pad1", ctypes.c_ushort),
-                    ("ipv4_addr", ctypes.c_byte * 4),
-                    ("ipv6_addr", ctypes.c_byte * 16),
-                    ("__pad2", ctypes.c_ulong)]
-
-    WSAStringToAddressA = ctypes.windll.ws2_32.WSAStringToAddressA
-    WSAAddressToStringA = ctypes.windll.ws2_32.WSAAddressToStringA
-
-    def inet_pton(address_family, ip_string):
-        addr = SockAddr()
-        ip_string = ip_string.encode('ascii')
-        addr.sa_family = address_family
-        addr_size = ctypes.c_int(ctypes.sizeof(addr))
-
-        if WSAStringToAddressA(
-            ip_string, address_family, None,
-            ctypes.byref(addr), ctypes.byref(addr_size)
-        ) != 0:
-            raise socket.error(ctypes.FormatError())
-
-        if address_family == socket.AF_INET:
-            return ctypes.string_at(addr.ipv4_addr, 4)
-        if address_family == socket.AF_INET6:
-            return ctypes.string_at(addr.ipv6_addr, 16)
-        raise socket.error('unknown address family')
 
 
 PY2 = (sys.version_info[0] == 2)
