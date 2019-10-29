65c7306b263bf8d09342660bbff33af3aa63bd9d
Wilfredo Sánchez <wsanchez@wsanchez.net>
2019-10-28 21:23:18 -0700

    Try harder to find a bogus family

--- src/hyperlink/test/test_socket.py
+++ src/hyperlink/test/test_socket.py
@@ -25,5 +25,12 @@ except ImportError:
                 inet_pton(socket.AF_INET6, "blah")
 
         def test_inet_pton_bogus_family(self):
+            # Find an integer not associated with a known address family
+            i = int(socket.AF_INET6) + 100
+            while True:
+                if i != socket.AF_INET and i != socket.AF_INET6:
+                    break
+                i += 100
+
             with self.assertRaises(socket.error):
-                inet_pton(-1, "blah")
+                inet_pton(i, "blah")
