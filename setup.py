ca2226450a233be53644de7c01cc84831fc71163
requires.io <support@requires.io>
2020-04-14 16:46:10 -0700

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -10,52 +10,47 @@ See the docs at http://hyperlink.readthedocs.io.
 from setuptools import find_packages, setup
 
 
-__author__ = 'Mahmoud Hashemi and Glyph Lefkowitz'
-__version__ = '19.0.1dev'
-__contact__ = 'mahmoud@hatnote.com'
-__url__ = 'https://github.com/python-hyper/hyperlink'
-__license__ = 'MIT'
+__author__ = "Mahmoud Hashemi and Glyph Lefkowitz"
+__version__ = "19.0.1dev"
+__contact__ = "mahmoud@hatnote.com"
+__url__ = "https://github.com/python-hyper/hyperlink"
+__license__ = "MIT"
 
 
-setup(name='hyperlink',
-      version=__version__,
-      description="A featureful, immutable, and correct URL for Python.",
-      long_description=__doc__,
-      author=__author__,
-      author_email=__contact__,
-      url=__url__,
-      packages=find_packages(where="src"),
-      package_dir={"": "src"},
-      package_data=dict(
-          hyperlink=[
-              "py.typed",
-          ],
-      ),
-      zip_safe=False,
-      license=__license__,
-      platforms='any',
-      install_requires=[
-          'idna>=2.5',
-          'typing ; python_version<"3.5"',
-      ],
-      python_requires='>=2.6, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
-      classifiers=[
-          'Topic :: Utilities',
-          'Intended Audience :: Developers',
-          'Topic :: Software Development :: Libraries',
-          'Development Status :: 5 - Production/Stable',
-          'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 2.6',
-          'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.4',
-          'Programming Language :: Python :: 3.5',
-          'Programming Language :: Python :: 3.6',
-          'Programming Language :: Python :: 3.7',
-          'Programming Language :: Python :: 3.8',
-          'Programming Language :: Python :: Implementation :: PyPy',
-          'License :: OSI Approved :: MIT License', ]
-      )
+setup(
+    name="hyperlink",
+    version=__version__,
+    description="A featureful, immutable, and correct URL for Python.",
+    long_description=__doc__,
+    author=__author__,
+    author_email=__contact__,
+    url=__url__,
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    package_data=dict(hyperlink=["py.typed",],),
+    zip_safe=False,
+    license=__license__,
+    platforms="any",
+    install_requires=["idna>=2.5", 'typing ; python_version<"3.5"',],
+    python_requires=">=2.6, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+    classifiers=[
+        "Topic :: Utilities",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries",
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
 
 """
 A brief checklist for release:
